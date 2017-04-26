1075e7dd39b33a1c1e6aa1d529bb8515d6458d5b
Juan Zsymchuk <juanangesilva@hotmail.com.ar>
2017-04-25 22:04:44 -0300

    add constraint to create civilization type

--- civilization.rb
+++ civilization.rb
@@ -1,7 +1,9 @@
 class Civilization
   attr_accessor :civilization_type, :armies
+  CIVILIZATION_TYPE = %w(england byzantium china).freeze
 
   def initialize(attrs = {})
+    raise 'invalid argument!' until CIVILIZATION_TYPE.include? attrs[:civilization_type]
     @civilization_type =  Kernel.const_get(attrs[:civilization_type].capitalize).new
     create_army
   end
